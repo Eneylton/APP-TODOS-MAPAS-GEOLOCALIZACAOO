ffde85320fb44181f11be85c62b7990fc863cfaf
eneylton <eneylton@hotmail.com>
2019-06-12 23:10:07 -0300

    Latitude / longitude - plugin geolocalizacao nativo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  lat:any;
+  lgn:any;
 
+  lat2:any;
+  lgn2:any;
+
+  constructor(public navCtrl: NavController,private geolocation: Geolocation) {
+    this.buscarLocal();
   }
 
+
+buscarLocal(){
+  this.geolocation.getCurrentPosition().then((resp) => {
+    this.lat = resp.coords.latitude
+    this.lgn = resp.coords.longitude
+   }).catch((error) => {
+     console.log('Error getting location', error);
+   });
+   
+   let watch = this.geolocation.watchPosition();
+   watch.subscribe((data) => {
+
+    this.lat2= data.coords.latitude
+    this.lgn2= data.coords.longitude
+   });
+
+
 }
+  
+  }
